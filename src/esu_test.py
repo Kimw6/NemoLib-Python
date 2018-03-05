ab8f9b0c0a948cc1bd33808c2056fd01e38a704c
Ryan Berge <ryanberge13@gmail.com>
2018-03-04 23:40:04 -0800

    Ryan: Expand top-level testing script

--- src/esu_test.py
+++ src/esu_test.py
@@ -1,6 +1,18 @@
-from NemoLib.esu.ryan_esu import _test
+from NemoLib.esu import ryan_esu
+from NemoLib.graph import undirected_graph
+
+import sys
 
 if __name__ == "__main__":
     '''Provide testing here.'''
+    if len(sys.argv) > 1:
+        graph_size = int(sys.argv[1])
+    else:
+        graph_size = 3
 
-    _test()
+    testGraph = undirected_graph.getSimpleTestGraph()
+
+    subgraphs = ryan_esu.getAllSubgraphs(testGraph, graph_size)
+
+    for graph in subgraphs:
+        print(graph)
